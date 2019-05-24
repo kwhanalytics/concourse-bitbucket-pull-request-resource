b8c620bc9ac2668c42d1a8786e1c9b89ca3db65a
Luis Alvarez <luis.alvarez@kwhanalytics.com>
2019-05-24 13:18:56 -0700

    added check on datetime

---
+++ scripts/utils.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def greater_than_days_cutoff(timestamp, cutoff):
+	""" Helper function to calculate if PR is past cutoff
+	"""
+
+	# Convert string to datetime object
+    last_update = datetime.strptime(timestamp[0:22], '%Y-%m-%dT%H:%M:%S.%f')
+
+    # Get the number of days since this PR has been last updated 
+    last_update_days = (datetime.now() - last_update).days
+
+    return last_update_days > cutoff
